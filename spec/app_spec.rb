b3ec895c759c2bc4f4d49e0ba3ee62e75debc86e
Mike Skalnik <mike.skalnik@gmail.com>
2012-12-29 21:38:45 -0600

    Test that all URLs passed are fetched

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,5 +9,28 @@ describe MakeMe::App do
       post '/print'
       expect(last_response.status).to_not eq(401)
     end
+
+    context 'when authed and unlocked' do
+      before do
+        auth
+        app.any_instance.stub(:locked? => false)
+        app.any_instance.stub(:lock!)
+      end
+
+      it 'fetches all the URLs given' do
+        urls = [
+                  'https://tinkercad.com/things/dP2KXaBxbNw/polysoup.stl',
+                  'https://tinkercad.com/things/9Ji3HC0Ukqq/polysoup.stl'
+               ]
+        urls.each_with_index do |url, index|
+          download = mock('download')
+          path = "#{MakeMe::App::FETCH_MODEL_FILE}.#{index}"
+          download.should_receive(:fetch)
+          MakeMe::Download.should_receive(:new).with(url, path).and_return(download)
+        end
+
+        post '/print', Yajl::Encoder.encode({:url => urls})
+      end
+    end
   end
 end
