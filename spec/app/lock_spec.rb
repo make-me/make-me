ed8b3a128de4854e01e90189b231a82c94b19c38
Mike Skalnik <mike.skalnik@gmail.com>
2012-12-29 18:46:25 -0600

    Add tests for DELETE /lock

--- spec/app/lock_spec.rb
+++ spec/app/lock_spec.rb
@@ -20,6 +20,35 @@ describe 'MakeMe::App Lock' do
     end
   end
 
+  describe 'DELETE /lock' do
+    it 'requires basic auth' do
+      delete '/lock'
+      expect(last_response.status).to eq(401)
+      auth
+      delete '/lock'
+      expect(last_response.status).to_not eq(401)
+    end
+
+    context "when user is authed and app is locked" do
+      before do
+        auth
+        app.any_instance.stub(:locked? => true)
+      end
+
+      it 'unlocks if the printer is not printing' do
+        File.stub(:exist?).with(MakeMe::App::PID_FILE).and_return(false)
+        MakeMe::Lock.any_instance.should_receive(:unlock!)
+        delete '/lock'
+      end
+
+      it 'returns 200 if it unlocks' do
+        File.stub(:exist?).with(MakeMe::App::PID_FILE).and_return(false)
+        delete '/lock'
+        expect(last_response.status).to eq(200)
+      end
+    end
+  end
+
   def auth
     authorize 'hubot', 'isalive'
   end
